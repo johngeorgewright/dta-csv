5989ea9682d6dda2b9534b34674a92bf6b89c38d
John Wright <johngeorge.wright@gmail.com>
2018-04-19 16:30:19 +0100

    feat: generalize script for other CSVs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ const csv = require('javascript-csv')
 const {createWriteStream, readFileSync} = require('fs')
 const {join: joinPath, resolve: resolvePath} = require('path')
 const request = require('request')
-const mkdirp = require('mkdirp')
 const {parallelLimit, retry} = require('async')
+const template = require('lodash.template')
 
-const BASE_URL = 'http://bbcsfx.acropolis.org.uk/assets/'
+// TODO: retrieve from CLI args
 const CSV_PATH = resolvePath(__dirname, '..', 'config', 'BBCSoundEffects.csv')
 const DOWNLOAD_PATH = resolvePath(__dirname, '..', 'downloads')
-const START_INDEX = 240
+const START_INDEX = 1120
 const BATCH_SIZE = 10
 const RETRY_NUM = 5
+const FILENAME_FORMAT = '<%=CDNumber%>.<%=tracknum%>.<%=description%>.<%=location%>'
+const URL_FORMAT = 'http://bbcsfx.acropolis.org.uk/assets/<%=location%>'
 
+const filenameTemplate = template(FILENAME_FORMAT)
+const urlTemplate = template(URL_FORMAT)
 const data = csv.toArrays(readFileSync(CSV_PATH).toString())
 const headers = data.shift()
 
@@ -22,16 +26,24 @@ const createDownloadStream = (url, filename) => (callback) => {
     .on('finish', callback)
 }
 
-const createDownload = ([location, description, secs, category, CDNumber, CDName, tracknum], index) => (callback) => {
-  const url = BASE_URL + location
-  const filename = joinPath(DOWNLOAD_PATH, `${CDNumber}.${tracknum}.${description}.${location}`)
+const createDownload = (data, index) => (callback) => {
+  const line = data.reduce(
+    (line, val, index) => ({
+      ...line,
+      [headers[index]]: val
+    }),
+    {}
+  )
+
+  const url = urlTemplate(line)
+  const filename = joinPath(DOWNLOAD_PATH, filenameTemplate(line))
 
   console.log(`downloading ${url}`)
 
   retry(RETRY_NUM, createDownloadStream(url, filename), err => {
     if (err) {
       console.log(`Failed to download ${url}`)
-      console.log(`Continue from index ${index}`)
+      console.log(`Continue from index ${START_INDEX + index}`)
     }
 
     callback(err)
